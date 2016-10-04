7e79a8768e6c65f2c92d248b475700b9ec0783a2
Roach-Sanderson <anthonysimonet@free.fr>
2016-10-04 19:22:14 +0200

    Etape A : Done

---
+++ src/index.php
@@ -0,0 +1,33 @@
+<?php
+  
+//lister atelier : GET
+//ajout & modification atelier : POST/DELETE
+
+/*
+$app->post('/ajout', function () use ($app){
+      $app->render('ajout.html', array());
+});
+$app->post('/modification', function () use ($app){
+      $app->render('liste.html', array());
+});
+$app->post('/lister', function () use ($app){
+      $app->render('liste.html', array());
+});
+*/
+
+//msg Antho : Mais t es un grand malade on est juste a l etape spec (A) !!!?
+
+
+  function liste_atelier(){
+
+  }
+
+  function ajout_atelier($id, $nom, $laboratoire, $lieu, $theme, $date){
+  
+  }
+
+  function modification_atelier($nom, $laboratoire, $lieu, $theme, $date){
+
+  }
+
+?>
